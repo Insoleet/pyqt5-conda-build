a6a4257ade43e0b2c4b09ce701dddd886584ff1f
Inso <insomniak.fr@gmail.com>
2016-01-09 17:03:52 +0100

    Remove qt5 + use Insoleet repository

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,7 +1,6 @@
 @ECHO ON
 
-git clone https://github.com/conda/conda-recipes.git
+git clone https://github.com/Insoleet/conda-recipes.git
 cd conda-recipes\\
-conda build qt5
 conda build sip
 conda build pyqt5
