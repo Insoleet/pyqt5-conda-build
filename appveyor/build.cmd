1ada04c721912e0285f24be744af783f426ff2dc
Insoleet <insomniak.fr@gmail.com>
2016-01-11 13:20:13 +0100

    Update build.cmd

--- appveyor/build.cmd
+++ appveyor/build.cmd
@@ -1,5 +1,3 @@
 @ECHO ON
-IF "%PYTHON_ARCH%"=="32" (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86) ELSE (ECHO "probably a 64bit build")
-IF "%PYTHON_ARCH%"=="64" (call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64) ELSE (ECHO "probably a 32bit build")
 conda build sip
 conda build pyqt5
